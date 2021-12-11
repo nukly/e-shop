0f3487c696afbe509fa70f8cd7997d192690e994
nukly <56589150+nukly@users.noreply.github.com>
2021-12-11 10:21:31 +0100

    Updated menu item with routing on click

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -7,15 +7,15 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
+
 const Directory = ({sections}) =>{
     return (
       <div className="directory-menu">
-        {sections.map(({ title, imageUrl, id, size }) => (
+        {sections.map(({ id, ...otherSectionProps }) => (
+          
           <MenuItem
             key={id}
-            title={title.toUpperCase()}
-            imageUrl={imageUrl}
-            size={size}
+            {...otherSectionProps}
           ></MenuItem>
         ))}
       </div>
